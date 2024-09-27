2b7f339c750b1c93092ff12c5a5901d79e9c4f78
SeiKdsu <g2121057@tcu.ac.jp>
2024-09-27 14:06:49 +0900

    ML_Gaussian

--- 12/NN-gaussian-process/1D_regression.py
+++ 12/NN-gaussian-process/1D_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+
 from nngp import NNGP
 
 L = 20
@@ -9,22 +10,20 @@ sigma_w_2 = 2
 sigma_b_2 = 1
 sigma_eps = 0.2
 
-# 修正された signal 関数
 def signal(x):
-    t1 = 20
-    t2 = - 20 * np.exp(- 0.2 * np.sqrt(1.0 / len(x) * np.sum(x ** 2)))
-    t3 = np.e
-    t4 = - np.exp(1.0 / len(x) * np.sum(np.cos(2 * np.pi * x)))
-    return t1 + t2 + t3 + t4
+    return 5*np.cos(0.5*x)
+    # t1 = 20
+    # t2 = - 20 * np.exp(- 0.2 * np.sqrt(1.0 / len(x) * np.sum(x ** 2)))
+    # t3 = np.e
+    # t4 = - np.exp(1.0 / len(x) * np.sum(np.cos(2 * np.pi * x)))
+    # return t1 + t2 + t3 + t4
 
-# トレーニングデータとテストデータ
 training_data = np.linspace(-10, 10, n_training).reshape((n_training, 1))
 training_output = signal(training_data)
 training_targets = training_output + sigma_eps * np.random.randn(n_training, 1)
 
 test_data = np.linspace(-10, 10, n_test).reshape(n_test, 1)
 
-# NNGP モデルの作成とトレーニング
 regression = NNGP(
     training_data,
     training_targets,
@@ -35,20 +34,13 @@ regression = NNGP(
 
 regression.train()
 
-# 予測と共分散行列
 predictions, covariance = regression.predict()
 variances = np.diag(covariance)
 
-# プロット
 plt.scatter(training_data, training_targets, label='train')
-
-# signal 関数の出力とプロット用データを1次元に変換
-plt.plot(test_data.flatten(), signal(test_data).flatten(), label='signal', ls='--', color='black', alpha=.7, lw=1)
-
-# predictions と test_data を 1次元に変換してから errorbar でプロット
-plt.errorbar(test_data.flatten(), predictions.flatten(), yerr=variances, color='red', label='prediction')
-
-# グラフの設定と保存
+plt.plot(test_data, signal(test_data), label='signal', ls='--', color='black', alpha=.7, lw=1)
+# predictions = predictions.flatten()
+# test_data = test_data.flatten()
+# plt.errorbar(test_data, predictions, yerr=variances, color='red', label='prediction')
 plt.legend()
 plt.savefig('1D_regression')
-plt.savefig('1D_reg.png')
