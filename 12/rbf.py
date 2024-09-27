2b385fb0337d42404da01cc9d00a7c87e4d1d665
SeiKdsu <g2121057@tcu.ac.jp>
2024-09-27 14:44:32 +0900

    ガウス過程と機械学習_chap3

---
+++ 12/rbf.py
@@ -0,0 +1,12 @@
+from sklearn.datasets import load_iris
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+X, y = load_iris(return_X_y=True)
+kernel = 1.0 * RBF(1.0)
+gpc = GaussianProcessClassifier(kernel=kernel,
+        random_state=0).fit(X, y)
+score = gpc.score(X, y)
+print(score)
+a = gpc.predict_proba(X[:2,:])
+print(a)
+print(gpc.kernel_)
