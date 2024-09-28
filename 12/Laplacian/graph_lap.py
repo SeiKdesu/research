de64f1ae2bb3391b8852d08aba9810a3ff7f53b4
SeiKdsu <g2121057@tcu.ac.jp>
2024-09-29 00:34:36 +0900

    install matlotlib

--- 12/Laplacian/graph_lap.py
+++ 12/Laplacian/graph_lap.py
@@ -1,4 +1,5 @@
 from LE import LE
+import numpy as np
 X = np.array([[2,3,4], [1,2,3]]) # nxd
 le = LE(X, dim = 3, k = 3, graph = 'k-nearest', weights = 'heat kernel', 
         sigma = 5, laplacian = 'symmetrized')
