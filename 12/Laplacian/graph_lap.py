0e7c383f051616f29deae885c3fe4b5846333f67
SeiKdsu <g2121057@tcu.ac.jp>
2024-09-29 00:24:24 +0900

    example_graph _lap

---
+++ 12/Laplacian/graph_lap.py
@@ -0,0 +1,5 @@
+from LE import LE
+X = np.array([[2,3,4], [1,2,3]]) # nxd
+le = LE(X, dim = 3, k = 3, graph = 'k-nearest', weights = 'heat kernel', 
+        sigma = 5, laplacian = 'symmetrized')
+Y = le.transform()
